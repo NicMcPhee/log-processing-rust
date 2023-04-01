f4fb34ba965cd41f6a7a00bbdabf34491431d20c
Nic McPhee <mcphee@morris.umn.edu>
2023-03-31 23:16:00 -0500

    Continue to fiddle with the code
    This gets us to where we're unzipping the `.tgz` files and getting the entries out of the `tar` archives. Now I need to get the lines from those and parse them into (possibly) failed login attempts.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -6,7 +6,7 @@ use log_processing_rust::{failed_login::FailedLogin, html_report::HtmlReport, lo
 fn main() -> Result<()> {
     // We skip the first argument because that's the name of the program.
     let log_file_names: Vec<String> = env::args().skip(1).collect();
-    let failed_logins: Vec<FailedLogin> = LogFile::process_log_files(&log_file_names)?;
+    let failed_logins: Vec<FailedLogin> = LogFile::process_tar_balls(&log_file_names)?;
     let report: HtmlReport = HtmlReport::new(failed_logins);
     report.save("failed_logins.html");
 
--- src/log_file.rs
+++ src/log_file.rs
@@ -1,20 +1,55 @@
-use anyhow::Result;
-use itertools::Itertools;
+use std::{
+    fs::{self, File},
+    io::{Read, BufReader, BufRead},
+    ops::Not,
+};
+
+use anyhow::{bail, Context, Error, Result};
+use flate2::read::GzDecoder;
+use itertools::{Either, Itertools};
+use tar::{Archive, Entry};
 
 use crate::failed_login::FailedLogin;
 
 pub struct LogFile {}
 
 impl LogFile {
-    pub fn process_log_files(log_files: &[String]) -> Result<Vec<FailedLogin>> {
-        log_files
+    pub fn process_tar_balls(tar_ball_names: &[String]) -> Result<Vec<FailedLogin>> {
+        tar_ball_names
             .iter()
-            .map(|filename| Self::process_log_file(filename))
+            .map(|tar_ball_name| Self::process_tar_ball(tar_ball_name))
             .flatten_ok()
             .collect::<Result<_>>()
     }
 
-    fn process_log_file(file: &str) -> Result<Vec<FailedLogin>> {
+    fn process_tar_ball(tar_ball_name: &str) -> Result<Vec<FailedLogin>> {
+        let tar_gz = File::open(tar_ball_name)?;
+        let tar = GzDecoder::new(tar_gz);
+        let mut archive = Archive::new(tar);
+        let (successes, failures): (Vec<_>, Vec<_>) = archive
+            .entries()
+            .with_context(|| format!("Failed to get the entry list from '{tar_ball_name}'"))?
+            .into_iter()
+            .partition_map(|r| match r {
+                Ok(v) => Either::Left(v),
+                Err(v) => Either::Right(v),
+            });
+
+        if failures.is_empty().not() {
+            bail!("These entries in {tar_ball_name} couldn't be extracted: {failures:?}");
+        }
+
+        Ok(successes.into_iter().map(|e| Self::process_log_file(e)).concat())
+    }
+
+    fn process_log_file(entry: Entry<GzDecoder<File>>) -> Vec<FailedLogin> {
+        let result = BufReader::new(entry)
+            .lines()
+            .map_ok(|line| line);
+        // let mut buffer = String::new();
+        // let _ = entry.read_to_string(&mut buffer);
+        //     buffer
+        // });
         todo!()
     }
 }
